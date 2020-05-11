2267b4684a83b94ee18f9fca1e644d3ec87abdd1
Paweł Kurdziel <pawelkurdziel.kontakt@gmail.com>
2020-05-11 15:01:06 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName = 'Paweł';
+const lastName = 'Kurdziel';
+const age = 23;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+console.log(`Cześć nazywam się ${firstName} ${lastName} i mam ${age} lata.`);
